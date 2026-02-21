b4151f14c07fdcbdd612a1cd76c575892305c3d1
rorazakhai24-ui <r.orazakhai24@apec.edu.kz>
2026-02-21 13:45:39 +0500

    Add files via upload

---
+++ -sharp_Rassul/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("1 – Online Store Order\n2 – Cinema Ticket\n3 – Gym Membership");
+        Console.Write("Your Choice: 2\n\n");
+
+        Console.Write("Movie Title: ");
+        string movieTitle = Console.ReadLine();
+
+        Console.Write("Number of Tickets: ");
+        int ticketsCount = int.Parse(Console.ReadLine());
+
+        Console.Write("Price per Ticket: ");
+        decimal ticketPrice = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Duration in hours: ");
+        double movieHours = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Screen Number: ");
+        int screenNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("First Letter of Row: ");
+        char rowLetter = char.Parse(Console.ReadLine());
+
+        Console.Write("Is 3D Movie (true/false): ");
+        bool is3DMovie = bool.Parse(Console.ReadLine());
+
+        decimal totalCost = ticketsCount * ticketPrice;
+        decimal costPerHour = totalCost / (decimal)movieHours;
+
+        Console.WriteLine("\nMovie: " + movieTitle + " | Tickets: " + ticketsCount + " | Price: " + ticketPrice);
+        Console.WriteLine("Total Cost: " + totalCost + " | Cost per Hour: " + costPerHour);
+
+        Console.WriteLine("\nTypes: " + ticketsCount.GetType() + ", " + movieHours.GetType() + ", " + is3DMovie.GetType());
+    }
+}
